9b6db2d088d0e9d3aae82ff0f6bd393096686278
Vladimir960107 <120406477+Vladimir960107@users.noreply.github.com>
2024-05-03 16:10:57 +0300

    User.cs (Design) was refactored

--- Kotova.Test1.ClientSide/User.Designer.cs
+++ Kotova.Test1.ClientSide/User.Designer.cs
@@ -31,6 +31,10 @@
             CheckForNewInstructions = new Button();
             label1 = new Label();
             label2 = new Label();
+            ListOfInstructions = new ListBox();
+            label3 = new Label();
+            button1 = new Button();
+            PassInstruction = new RadioButton();
             SuspendLayout();
             // 
             // CheckForNewInstructions
@@ -60,11 +64,54 @@
             label2.Size = new Size(0, 15);
             label2.TabIndex = 23;
             // 
+            // ListOfInstructions
+            // 
+            ListOfInstructions.FormattingEnabled = true;
+            ListOfInstructions.ItemHeight = 15;
+            ListOfInstructions.Location = new Point(214, 170);
+            ListOfInstructions.Name = "ListOfInstructions";
+            ListOfInstructions.Size = new Size(430, 229);
+            ListOfInstructions.TabIndex = 24;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(214, 127);
+            label3.Name = "label3";
+            label3.Size = new Size(201, 15);
+            label3.TabIndex = 25;
+            label3.Text = "Лист непройденных инструктажей:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(662, 170);
+            button1.Name = "button1";
+            button1.Size = new Size(111, 229);
+            button1.TabIndex = 26;
+            button1.Text = "Гиперссылка на инструктаж";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // PassInstruction
+            // 
+            PassInstruction.AutoSize = true;
+            PassInstruction.Enabled = false;
+            PassInstruction.Location = new Point(214, 419);
+            PassInstruction.Name = "PassInstruction";
+            PassInstruction.Size = new Size(244, 19);
+            PassInstruction.TabIndex = 27;
+            PassInstruction.TabStop = true;
+            PassInstruction.Text = "Подтверждаю, что интсруктаж пройден";
+            PassInstruction.UseVisualStyleBackColor = true;
+            // 
             // User
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(PassInstruction);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(ListOfInstructions);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(CheckForNewInstructions);
@@ -79,5 +126,9 @@
         private Button CheckForNewInstructions;
         private Label label1;
         private Label label2;
+        private ListBox ListOfInstructions;
+        private Label label3;
+        private Button button1;
+        private RadioButton PassInstruction;
     }
 }
