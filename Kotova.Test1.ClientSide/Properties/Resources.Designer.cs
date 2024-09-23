60d898a492b8bfd8da2cfe674b2e475bed31920a
Vladimir960107 <120406477+Vladimir960107@users.noreply.github.com>
2024-09-23 11:32:00 +0300

    Merged all the projects into 1

--- Kotova.Test1.ClientSide/Properties/Resources.Designer.cs
+++ Kotova.Test1.ClientSide/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Kotova.Test1.ClientSide.Properties {
+namespace Kotova.ClientSide.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Kotova.Test1.ClientSide.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Kotova.Test1.ClientSide.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Kotova.ClientSide.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
